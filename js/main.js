f18bfc8e7830557c1ca0185f73050521e41da458
korzkz <128216993+korzkz@users.noreply.github.com>
2023-04-27 22:13:48 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var ball = document.getElementsByClassName('ball')[0]
+var points = document.getElementsByClassName('points')[0]
+var points_num = 0
+ball.addEventListener('click', () => {
+    ball.style.top = Math.floor(Math.random() * (350 - 0 + 1) + 0) + "px";
+    ball.style.left = Math.floor(Math.random() * (350 - 0 + 1) + 0) + "px";
+    points_num += 100
+    points.innerHTML = 'Points: ' + points_num;
+})
