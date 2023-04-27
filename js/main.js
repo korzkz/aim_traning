5b46f4c146f4f70ccd893dcc281704072e461cd4
korzkz <128216993+korzkz@users.noreply.github.com>
2023-04-27 22:44:10 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ var points_num = 0
 ball.addEventListener('click', () => {
     ball.style.top = Math.floor(Math.random() * (350 - 0 + 1) + 0) + "px";
     ball.style.left = Math.floor(Math.random() * (350 - 0 + 1) + 0) + "px";
-    points_num += 100
+    points_num += 200
+    points.innerHTML = 'Points: ' + points_num;
+})
+document.getElementsByClassName('frame')[0].addEventListener('click', () => {
+    points_num -= 100
     points.innerHTML = 'Points: ' + points_num;
 })
